38e057f382966ec4531753bbe78793aea37d017a
Sipun Kumar Nanda <98945228+Sipunkumarnanda21@users.noreply.github.com>
2024-05-13 12:26:37 +0000

    Conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,1 +1,74 @@
-console.log("Conversion Operation in Js ");
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber =Number(score) ;
+
+console.log(typeof valueInNumber);
+
+let mark = "65ha" ;
+let markInNumber = Number(mark);
+console.log(typeof markInNumber);
+console.log(markInNumber);
+console.log(typeof NaN);  // Typeof NaN Is Number
+
+
+
+   /*  In null Cases   */
+
+let mark2 = null
+console.log(mark2);             // null
+console.log(typeof mark2);     // Object
+let mark2InNumber = Number(mark2)
+console.log(typeof mark2InNumber);           // number
+console.log(mark2InNumber);                  // 0
+
+
+
+  /*  In Undefined Cases   */
+
+let score2 = undefined
+console.log(score2);                       // undefined
+console.log(typeof score2);                // undefined
+
+let score2InNumber = Number(score2)
+console.log(score2InNumber);            // NaN
+console.log(typeof score2InNumber);     // Number
+
+
+/*  In boolean Cases   */
+let bool = true
+console.log(typeof bool);         // boolean
+
+let boolInNumber = Number(bool)    
+console.log(boolInNumber);            // 1
+console.log(typeof boolInNumber);    // number
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 false => 0 
+
+/*  In boolean Cases   */
+
+let isLoggedIn = 1
+
+let isLoggedInInBoolean = Boolean(isLoggedIn)
+
+console.log(isLoggedInInBoolean);   // true
+console.log(typeof isLoggedInInBoolean);  // Boolean
+
+/*  In String To boolean Cases   */
+
+let isLoggedIn2 ="Sipun";    // If its empty ("") => false , if "Sipun" / anything inside "" => true
+
+let isLoggedIn2InBoolean = Boolean(isLoggedIn2)
+console.log(isLoggedIn2InBoolean);
+console.log(typeof isLoggedIn2InBoolean);   
+
+
+let someNumber = 33 
+let stringNumber = String(someNumber) 
+console.log(stringNumber);
+console.log(typeof stringNumber);
