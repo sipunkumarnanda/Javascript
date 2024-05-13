776344bbdc0b3e4b696dbe209adb34631488047a
Sipun Kumar Nanda <98945228+Sipunkumarnanda21@users.noreply.github.com>
2024-05-13 12:40:40 +0000

    Conversion and Operations In JS datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -72,3 +72,28 @@ let someNumber = 33
 let stringNumber = String(someNumber) 
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+/* ****************** Operations ******************* */
+console.log("****************** Operations *******************");
+
+let value = 3 
+let negValue = -value 
+console.log(negValue);
+console.log(typeof negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+
+let str1 = "Hello"
+let str2 = " Sipun"
+console.log(str1 + str2) ;
+
+console.log(1 + "2");
+
+console.log("1" + 2 + 2);   // 122
+console.log(1 + 2 + "2");   // 32
