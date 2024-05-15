cc184f362d61136f54b034dad35bb891ca113377
Sipun Kumar Nanda <98945228+Sipunkumarnanda21@users.noreply.github.com>
2024-05-15 10:56:21 +0000

    Object In Js

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,73 @@
+// const tinderUser = new Object()   // singleton Object 
+// const tinderUser = {}            // Non-Singleton Object
+
+
+const tinderUser = new Object()
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email : "some@gmail.com" ,
+    fullName : {
+        usersFullName : {
+            firstName : "sipun" ,
+            lastName : "Nanda"
+        }
+    }
+}
+
+// console.log(regularUser.fullName);
+// console.log(regularUser.fullName.usersFullName.lastName);
+
+const obj1 ={
+    1 : "a" ,
+    2 : "b"
+}
+const obj2 ={
+    3 : "c" ,
+    4 : "d"
+}
+const obj4 ={
+    5 : "E" ,
+    6 : "F"
+}
+
+// const obj3 = {obj1 , obj2}   // Wrong Way
+
+const obj3 = Object.assign({}, obj1 , obj2 , obj4)   //The first is the target object, where all objects go. 
+                                                    // obj3 is created by copying properties from obj1, obj2, and obj4 into a new object ({})
+// console.log(obj3);
+
+
+const realObj = { ...obj1 , ...obj2 , ...obj4 }
+console.log(realObj);                 // { '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'E', '6': 'F' }
+
+const users = [
+    {
+        id: 1 ,
+        email : "h@gmail.com" ,
+    } ,
+    {
+        id: 1 ,
+        email : "h@gmail.com" ,
+    } ,
+    {
+        id: 1 ,
+        email : "h@gmail.com" ,
+    }
+]
+
+users[1].email
+
+console.log(tinderUser)                               // { id: '123abc', name: 'Sammy', isLoggedIn: false }
+ 
+console.log(Object.keys(tinderUser))                     // [ 'id', 'name', 'isLoggedIn' ]
+console.log(Object.values(tinderUser))                   // [ '123abc', 'Sammy', false ]
+console.log(Object.entries(tinderUser))                 // [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));       // true
+console.log(tinderUser.hasOwnProperty('isLoggedInn'));     // false
