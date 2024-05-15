52ab64d6239659e82da4baa9c64c19918c3538ad
Sipun Kumar Nanda <98945228+Sipunkumarnanda21@users.noreply.github.com>
2024-05-15 04:22:57 +0000

    Comments added

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -4,9 +4,9 @@ const myHeros = ["Shaktiman" , "naagraj"]
 
 const myArr2 = new Array(1, 2, 3 , 4 )
 
-console.log(myArray[0]);
-console.log(myArray);
-console.log(myArr2);
+console.log(myArray[0]);    // 0
+console.log(myArray);       // [ 0, 1, 2, 3, 4, 5 ]
+console.log(myArr2);        // [ 1, 2, 3, 4 ]
 
 
 // Array methods
@@ -14,7 +14,7 @@ console.log(myArr2);
 myArray.push(6)
 myArray.push(7)
 myArray.pop()
-console.log(myArray);
+console.log(myArray);   // [0, 1, 2, 3, 4, 5, 6 ]
 
 myArray.unshift(9)  // [ 9, 0, 1, 2, 3, 4, 5, 6 ]
 console.log(myArray) 
@@ -27,22 +27,22 @@ console.log(myArray.includes(9)); // False
 console.log(myArray.indexOf(19));   // -1
 
 const newArr = myArray.join()
-console.log(newArr);
-console.log(typeof newArr);
+console.log(newArr);           // 0,1,2,3,4,5,6
+console.log(typeof newArr);   // string
 
 
 // Slice , Splice
 
-console.log("A " , myArray);
+console.log("A " , myArray);     // A  [ 0, 1, 2, 3,4, 5, 6 ]
 
 const myn1 = myArray.slice(1,3)
 
-console.log(myn1);
-console.log("B " , myArray);
+console.log(myn1);                // [ 1, 2 ]
+console.log("B " , myArray);     // B  [ 0, 1, 2, 3, 4, 5, 6 ]
 
 const myn2 = myArray.splice(1,3) 
-console.log(myn2);
-console.log("c " , myArray);  
+console.log(myn2);               // [ 1, 2, 3 ]
+console.log("c " , myArray);    // c  [ 0, 4, 5, 6 ]
 
 /* 
 Note : 
--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -10,22 +10,22 @@ const dc_heros = ["Superman" , "Flash" , "Batman"]
 // console.log(allHeros);    // [ 'thor', 'Ironman', 'Spiderman', 'Superman', 'Flash', 'Batman' ]
 
 const allNewHeros = [...marvel_heros, ...dc_heros]
-console.log(allNewHeros);
+console.log(allNewHeros);    // [ 'thor', 'Ironman', 'Spiderman', 'Superman', 'Flash', 'Batman' ]
 
 
 const anotherArray = [1,2,3, [4,5,6] , 7, [6,7 , [4,5]]]
 
-const realAnotherArray = anotherArray.flat(Infinity)
-console.log(realAnotherArray);
+const realAnotherArray = anotherArray.flat(Infinity)   // If Array Inside Array 
+console.log(realAnotherArray);     // [1, 2, 3, 4, 5,6, 7, 6, 7, 4,5]
 
 
-console.log(Array.isArray("Sipun"));
-console.log(Array.from("Sipun"));
-console.log(Array.from({name: "Sipun"}));  // Intresting Interview question 
+console.log(Array.isArray("Sipun"));        // false
+console.log(Array.from("Sipun"));          // [ 'S', 'i', 'p', 'u', 'n' ]
+console.log(Array.from({name: "Sipun"}));  // Intresting Interview question (Blank Array - [] )
 
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1,score2,score3));
+console.log(Array.of(score1,score2,score3));    // [ 100, 200, 300 ]
